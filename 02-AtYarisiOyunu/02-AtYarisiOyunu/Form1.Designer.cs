7f33c356a5db75c698275d4cdc434aa86dff58d4
Nuray <106771227+nrymrhn@users.noreply.github.com>
2022-06-22 19:40:18 +0300

    At Yarisi Oyunu

---
+++ 02-AtYarisiOyunu/02-AtYarisiOyunu/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace _02_AtYarisiOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlBitis = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Firebrick;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(13, 436);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(137, 58);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlBitis
+            // 
+            this.pnlBitis.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pnlBitis.Location = new System.Drawing.Point(777, -3);
+            this.pnlBitis.Name = "pnlBitis";
+            this.pnlBitis.Size = new System.Drawing.Size(24, 503);
+            this.pnlBitis.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::_02_AtYarisiOyunu.Properties.Resources.gunun_gif_i_1299032;
+            this.pictureBox3.Location = new System.Drawing.Point(5, 301);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(168, 109);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_02_AtYarisiOyunu.Properties.Resources.AT_YARIŞI;
+            this.pictureBox2.Location = new System.Drawing.Point(5, 157);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 109);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_02_AtYarisiOyunu.Properties.Resources.at_gif;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(5, 130);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(775, 15);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Location = new System.Drawing.Point(0, 134);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(754, 15);
+            this.panel3.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Location = new System.Drawing.Point(5, 278);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(775, 15);
+            this.panel4.TabIndex = 7;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel5.Location = new System.Drawing.Point(0, 134);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(754, 15);
+            this.panel5.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Location = new System.Drawing.Point(5, 416);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 15);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel6.Location = new System.Drawing.Point(0, 134);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(754, 15);
+            this.panel6.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlBitis);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "AT YARIŞI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlBitis;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
+
---
+++ 02-AtYarisiOyunu/02-AtYarisiOyunu/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_AtYarisiOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 100;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int atHizi1 = rnd.Next(15);
+            int atHizi2 = rnd.Next(15);
+            int atHizi3 = rnd.Next(15);
+
+            pictureBox1.Location = new Point(pictureBox1.Location.X + atHizi1, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point(pictureBox2.Location.X + atHizi2, pictureBox2.Location.Y);
+            pictureBox3.Location = new Point(pictureBox3.Location.X + atHizi1, pictureBox3.Location.Y);
+
+            if (pictureBox1.Location.X+pictureBox1.Width>=pnlBitis.Location.X)
+            {
+                this.Text = "Yarış Bitti";
+                timer1.Stop();
+                MessageBox.Show("Tebrikler 1. At Kazanmıştır.","BİLGİ");
+            }
+            else if (pictureBox2.Location.X + pictureBox2.Width >= pnlBitis.Location.X)
+            {
+                this.Text = "Yarış Bitti"; ;
+                timer1.Stop();
+                MessageBox.Show("Tebrikler 2. At Kazanmıştır.","BİLGİ");
+            }
+            else if (pictureBox3.Location.X + pictureBox3.Width >= pnlBitis.Location.X)
+            {
+                this.Text = "Yarış Bitti";
+                timer1.Stop();
+                MessageBox.Show("Tebrikler 3. At Kazanmıştır.","BİLGİ");
+            }
+        }
+    }
+}
